bd4bdb9e0c294871279c82082541a764dddf2c3c
EY shubham <Shubham.Gupta13@in.ey.com>
2024-10-10 13:19:47 +0530

    changes

--- db/connection.js
+++ db/connection.js
@@ -3,9 +3,6 @@ const dotenv = require('dotenv'); // For managing environment variables
 dotenv.config(); // Load environment variables from .env file
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  poolSize: 10, // Maintain up to 10 socket connections
   serverSelectionTimeoutMS: 30000 // Increase timeout to 30 seconds
 })
   .then(() => {
