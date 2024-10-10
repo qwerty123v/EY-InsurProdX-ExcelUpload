1ea328559bd2cf9325233bc16e30f5490046d594
EY shubham <Shubham.Gupta13@in.ey.com>
2024-10-10 19:26:40 +0530

    added cors

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ dotenv.config(); // Load environment variables from .env file
 const PORT = parseInt(process.env.PORT) || 8080;
 const route=require('./routes/uploadRoutes');
 const cors= require('cors');
-app.use(cors())
+
+const corsOptions = {
+  origin: 'https://bd3b2096-4a05-4bd3-816e-8bdada54b7d6.e1-us-east-azure.choreoapps.dev',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-requested-with'],
+};
+
+app.use(cors(corsOptions));
+// app.use(cors())
 
 app.use('/excelupload',route);
 
