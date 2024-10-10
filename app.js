384dc7cb2945c31d12fd229367a2d56fd07e647a
EY-Shubham <shubham.gupta13@in.ey.com>
2024-10-10 20:17:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,14 +7,19 @@ const PORT = parseInt(process.env.PORT) || 8080;
 const route=require('./routes/uploadRoutes');
 const cors= require('cors');
 
-const corsOptions = {
-  origin: 'https://bd3b2096-4a05-4bd3-816e-8bdada54b7d6.e1-us-east-azure.choreoapps.dev',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-requested-with'],
-};
+// const corsOptions = {
+//   origin: 'https://bd3b2096-4a05-4bd3-816e-8bdada54b7d6.e1-us-east-azure.choreoapps.dev',
+//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//   allowedHeaders: ['Content-Type', 'Authorization', 'x-requested-with'],
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 // app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
+  allowedHeaders: '*',
+}));
 
 app.use('/excelupload',route);
 
