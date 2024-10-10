eca23ca032d7b5548aa82fc5191587dc0e12b260
EY shubham <Shubham.Gupta13@in.ey.com>
2024-10-10 11:01:32 +0530

    adding new file for choreo deployement

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ require("./db/connection");
 const app = express();
 const dotenv = require('dotenv'); // For managing environment variables
 dotenv.config(); // Load environment variables from .env file
+const PORT = parseInt(process.env.PORT) || 8080;
 const route=require('./routes/uploadRoutes');
 const cors= require('cors');
 app.use(cors())
 
 app.use('/excelupload',route);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening on port ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
